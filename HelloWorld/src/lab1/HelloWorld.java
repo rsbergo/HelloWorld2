506a8d9e071cf2ac759bf9f6eb95a471cf6c1e3a
Rafael Bergo <rafael.desouzabergo@maine.edu>
2020-01-23 19:08:03 -0500

    Add Hello World!

---
+++ HelloWorld/src/lab1/HelloWorld.java
@@ -0,0 +1,22 @@
+package lab1;
+
+/**
+ * COS 420 - Lab 1: A Git HelloWorld Tour
+ * http://www.cs.usm.maine.edu/~macleod/courses/cos420/Spring2019/Lab1.html
+ *
+ * Create java program that displays "Hello World" on your machines, create a new repository for that program on your
+ * laptop/machine, add to code to staging and commit the code, & then upload this repository to the HelloWorld2
+ * repository.
+ */
+public class HelloWorld
+{
+    /**
+     * Entry point for application.
+     * 
+     * @param args main program arguments, currently not used
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
